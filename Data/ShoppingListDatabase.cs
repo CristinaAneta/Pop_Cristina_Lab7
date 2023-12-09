4136655647edc5aced8eca3a5729a91db5c1b989
CRISTINA-ANETA POP <cristina.aneta.pop@stud.ubbcluj.ro>
2023-12-09 17:37:20 +0200

    incarcare

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -9,14 +9,40 @@ using Pop_Cristina_Lab7.Models;
 
 namespace Pop_Cristina_Lab7.Data
 {
-    public class ShoppingListDatabase
+    public class ShoppingListDataBase
     {
         readonly SQLiteAsyncConnection _database;
-        public ShoppingListDatabase(string dbPath)
+        public ShoppingListDataBase(string dbPath)
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
+
         }
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+
         public Task<List<ShopList>> GetShopListsAsync()
         {
             return _database.Table<ShopList>().ToListAsync();
@@ -42,5 +68,25 @@ namespace Pop_Cristina_Lab7.Data
         {
             return _database.DeleteAsync(slist);
         }
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+
     }
 }
--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -2,24 +2,43 @@ using Pop_Cristina_Lab7.Models;
 
 
 namespace Pop_Cristina_Lab7
-{ 
-public partial class ListPage : ContentPage
 {
-    public ListPage()
+
+    public partial class ListPage : ContentPage
     {
-        InitializeComponent();
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
     }
-    async void OnSaveButtonClicked(object sender, EventArgs e)
-    {
-        var slist = (ShopList)BindingContext;
-        slist.Date = DateTime.UtcNow;
-        await App.Database.SaveShopListAsync(slist);
-        await Navigation.PopAsync();
-    }
-    async void OnDeleteButtonClicked(object sender, EventArgs e)
-    {
-        var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
-        await Navigation.PopAsync();
-    }
-} }
+}
